c1b8ac4db6d88de627fb446bdf93c6dcfd1b32d3
davidalpha <david.alpha@gmail.com>
2015-09-14 23:30:14 +0200

    first build

---
+++ scripts/vert_area.gd
@@ -0,0 +1,19 @@
+
+extends Area2D
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	set_process_input(true)
+	
+func _input(event):
+   # if user left clicks
+	if (event.type == InputEvent.MOUSE_BUTTON and event.button_index == 1):
+	   	if event.is_pressed():
+	       self.set_space_override_mode(1)
+	    else:
+	       self.set_space_override_mode(0)
+	elif event.type == InputEvent.MOUSE_MOTION:
+		 self.set_pos(Vector2(event.x,event.y))
